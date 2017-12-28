00974a66aaec6cb4bcb8cb1df237fea21316f094
repjarms <jeffarn12@gmail.com>
2017-12-28 13:10:04 -0500

    add Project stub

---
+++ src/components/Project.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="project">
+    <img src="#" />
+    <div class="project_details">
+      <h3>{{title}}</h3>
+      <p>{{description}}</p>
+      <div class="project_buttonlist">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Project',
+  props: ['title', 'description', 'githubLink', 'appLink', 'image'],
+};
+</script>
+
+<style scoped lang="scss">
+.project {
+  color: black;
+}
+</style>
