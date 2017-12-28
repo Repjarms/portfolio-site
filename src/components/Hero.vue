c7e2a754b1c6217365ce45c6718998ebde1d1709
repjarms <jeffarn12@gmail.com>
2017-12-28 00:13:23 -0500

    add component stubs

---
+++ src/components/Hero.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="hero">
+   <header class="hero_header">
+     <ul class="hero_linklist">
+      <li>Projects</li> 
+      <li>Blogs</li>
+      <li>Github</li>
+      <li>More</li>
+     </ul>
+   </header> 
+   <div class="hero_text">
+     <h1>My Name Is Jeff Arn</h1>
+     <h2>I make webapps and embedded systems</h2>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hero',
+};
+</script>
+
+<style scoped lang="scss">
+@import '../main.scss';
+
+.hero {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  background: $gray-10;
+  color: white;
+  height: 100vh;
+}
+
+.hero_header {
+  flex: 0 1 100%;
+  height: 10%;
+  background: $gray-20;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.hero_linklist {
+  display: flex;
+  margin-right: 1em;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  color: $teal;
+
+  li {
+    &:hover {
+      color: white;
+    }
+  }
+}
+
+.hero_text {
+  flex: 0 1 100%;
+  text-align: center;
+}
+</style>
