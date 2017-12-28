3b3d225c9a930b7018e96bee19bb25c1aeea82a2
repjarms <jeffarn12@gmail.com>
2017-12-28 13:25:41 -0500

    Style Project component

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="project">
-    <img src="#" />
+    <img src="#" class="project_image" />
     <div class="project_details">
       <h3>{{title}}</h3>
       <p>{{description}}</p>
@@ -18,7 +18,43 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '../colors';
+
 .project {
+  padding: 10%;
   color: black;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: flex-start;
+
+  &:nth-child(2) {
+    background: $teal;
+  }
+
+  &:nth-child(4) {
+    background: $gray-20;
+    color: white;
+  }
+
+  &:nth-child(2n) {
+    img {
+      order: 1;
+    }
+
+    div {
+      order: 0;
+    }
+  }
+}
+
+.project_image {
+  flex: 0 1 50%;
+}
+
+.project_details {
+  flex: 0 1 50%;
 }
 </style>
