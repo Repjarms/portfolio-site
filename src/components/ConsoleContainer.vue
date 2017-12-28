60579c2dfe6866d487786671a8d4046beeb755c9
repjarms <jeffarn12@gmail.com>
2017-12-28 11:25:05 -0500

    add stub for ConsoleContainer component

---
+++ src/components/ConsoleContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="console">
+    <p>option list placeholder</p>
+    <div class="console_window">
+      <p>Console pane placeholder</p> 
+      <p>Console pane placeholder</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'console',
+} 
+</script>
+
+<style scoped lang="scss">
+@import '../main.scss';
+
+.console {
+  flex: 0 1 80%;
+  margin: 5% 0;
+}
+
+.console_window {
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+}
+</style>
