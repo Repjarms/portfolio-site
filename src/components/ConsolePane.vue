6b580ee359f175b32b751563e6a23ef1c6624b42
repjarms <jeffarn12@gmail.com>
2017-12-28 11:40:37 -0500

    add stub for ConsolePane

---
+++ src/components/ConsolePane.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="console_pane">
+   Console pane placeholder 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConsolePane'
+};
+</script>
+
+<style scoped lang="scss">
+.console_pane {
+  flex: 0 1 50%;
+  padding: 1em;
+}
+
+  
+</style>
